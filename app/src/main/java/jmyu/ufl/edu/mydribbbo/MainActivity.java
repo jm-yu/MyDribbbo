21a85f4376af180c462704dd9d8669ef7d1d3fc0
jmyu1994 <jmyu1994@gmail.com>
2018-06-26 17:31:19 -0400

    set up navigation drawer click event

--- app/src/main/java/jmyu/ufl/edu/mydribbbo/MainActivity.java
+++ app/src/main/java/jmyu/ufl/edu/mydribbbo/MainActivity.java
@@ -1,14 +1,70 @@
 package jmyu.ufl.edu.mydribbbo;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem item) {
+                        // set item as selected to persist highlight
+                        item.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+                        switch (item.getItemId()) {
+                            case R.id.drawer_item_home:
+                                setTitle(R.string.title_home);
+                                break;
+                            case R.id.drawer_item_likes:
+                                setTitle(R.string.title_likes);
+                                break;
+                            case R.id.drawer_item_buckets:
+                                setTitle(R.string.title_buckets);
+                                break;
+                        }
+
+                        return false;
+                    }
+                });
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .commit();
+        }
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
