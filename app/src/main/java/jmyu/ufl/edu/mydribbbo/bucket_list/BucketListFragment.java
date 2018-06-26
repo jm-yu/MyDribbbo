73d799fcf0bc2a591eb2595bfbaa16219d735639
jmyu1994 <jmyu1994@gmail.com>
2018-06-26 17:46:51 -0400

    set up navigation drawer click event and test it

---
+++ app/src/main/java/jmyu/ufl/edu/mydribbbo/bucket_list/BucketListFragment.java
@@ -0,0 +1,8 @@
+package jmyu.ufl.edu.mydribbbo.bucket_list;
+
+/**
+ * Created by jmyu on 6/26/18.
+ */
+
+public class BucketListFragment {
+}
---
+++ app/src/main/java/jmyu/ufl/edu/mydribbbo/shot_list/ShotListFragment.java
@@ -0,0 +1,8 @@
+package jmyu.ufl.edu.mydribbbo.shot_list;
+
+/**
+ * Created by jmyu on 6/26/18.
+ */
+
+public class ShotListFragment {
+}
