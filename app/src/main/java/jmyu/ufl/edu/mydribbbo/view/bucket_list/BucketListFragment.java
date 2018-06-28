790607c9aef6c5c5bf90c7f1bccd8dfb803b6781
jmyu1994 <jmyu1994@gmail.com>
2018-06-28 12:10:27 -0400

    add bucket list settings

--- app/src/main/java/jmyu/ufl/edu/mydribbbo/view/bucket_list/BucketListFragment.java
+++ app/src/main/java/jmyu/ufl/edu/mydribbbo/view/bucket_list/BucketListFragment.java
@@ -1,6 +1,23 @@
 package jmyu.ufl.edu.mydribbbo.view.bucket_list;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import jmyu.ufl.edu.mydribbbo.R;
+import jmyu.ufl.edu.mydribbbo.model.Bucket;
 
 /**
  * Created by jmyu on 6/26/18.
@@ -8,7 +25,35 @@ import android.support.v4.app.Fragment;
 
 public class BucketListFragment extends Fragment {
 
+    @BindView(R.id.recycler_view) RecyclerView recyclerView;
+
     public static BucketListFragment newInstance() {
         return new BucketListFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view  = inflater.inflate(R.layout.bucket_list_item, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(new BucketListAdaper(fakeData()));
+    }
+
+    private List<Bucket> fakeData() {
+        List<Bucket> bucketList = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 20; ++i) {
+            Bucket bucket = new Bucket();
+            bucket.name = "Bucket" + i;
+            bucket.shots_count = random.nextInt(10);
+            bucketList.add(bucket);
+        }
+        return bucketList;
     }
 }
